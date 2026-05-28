01f7c12efd602be0446afc19307709293732ab85
James Thompson <jamesgct96@gmail.com>
2026-05-28 12:16:36 -0400

    shutdown complete

---
+++ submission.sql
@@ -0,0 +1,7 @@
+select * from forum_posts; - smart-money-44
+select * from forum_accounts where username ilike 'smart%'; - Brad Steele
+select * from forum_accounts where last_name ilike 'steele'; sharp-engine-57, stinky-tofu-98, smart-money-44
+select * from emptystack_accounts where last_name ilike 'steele'; Andrew Steele UN: triple-cart-38 PW: password456
+select * from emptystack_messages; your-boss-99 
+select * from emptystack_accounts; notagaincarter  | Skylar      | Singer 
+select * from emptystack_projects;  DczE0v2b | TAXI
